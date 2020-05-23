2624d46c3a46258c103d3d712c94d62577303f2d
Minsu Lee <amond@amond.net>
2020-05-24 08:12:52 +0900

    http/1 fallback test

--- test/all_test.dart
+++ test/all_test.dart
@@ -100,6 +100,12 @@ void main() {
     }
   });
 
+  test('http/1 site fallback test', () async {
+    var response = await client.get('https://naver.com');
+    expect(response.statusCode, 200);
+    expect(response.body, isNotEmpty);
+  });
+
   group('uncompress', () {
     test('gzip', () async {
       var response = await client.get(baseUrl.replace(path: 'compress'),
